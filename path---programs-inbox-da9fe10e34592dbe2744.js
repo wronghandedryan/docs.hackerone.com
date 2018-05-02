07930514aff916edd00c60e3c5af85b410d77ed7
Lars Bekkema <lars.bekkema+gitlab@gmail.com>
2018-05-02 13:09:48 +0200

    Updates

--- path---programs-inbox-da9fe10e34592dbe2744.js
+++ path---programs-inbox-da9fe10e34592dbe2744.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x71cc771c041c],{439:function(t,o){t.exports={data:{markdownRemark:{html:"<p>Article about inbox overview</p>",frontmatter:{path:"/programs/inbox",title:"Inbox"}}},pathContext:{}}}});
+webpackJsonp([0x71cc771c041c],{448:function(t,o){t.exports={data:{markdownRemark:{html:"<p>Article about inbox overview</p>",frontmatter:{path:"/programs/inbox",title:"Inbox"}}},pathContext:{}}}});
 //# sourceMappingURL=path---programs-inbox-da9fe10e34592dbe2744.js.map
