1965b44d66919ca3e6070d6c0dbd0fbe981fa91a
Lars Bekkema <lars.bekkema+gitlab@gmail.com>
2018-05-02 15:02:45 +0200

    Updates

---
+++ path---programs-automation-html-2d1cc71b714c8e7fc4f0.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xaad46529723e],{427:function(t,o){t.exports={data:{markdownRemark:{html:"<p>HackerOne offers various automated features to simplify your workflow. You can configure:</p>\n<ul>\n<li>Default common responses to reports</li>\n<li>Triggers that'll automate an action</li>\n<li>Integrations with other third party services </li>\n<li>Hackbot notifications</li>\n</ul>",frontmatter:{path:"/programs/automation.html",id:"programs/automation",title:"Automation"}}},pathContext:{}}}});
+//# sourceMappingURL=path---programs-automation-html-2d1cc71b714c8e7fc4f0.js.map
