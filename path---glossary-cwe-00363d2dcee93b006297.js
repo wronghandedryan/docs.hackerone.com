e3ce8ad92258643522c60a09b3ee2bfee917cf7c
Lars <lars.bekkema@gmail.com>
2019-02-05 09:40:59 -0800

    Updates

--- path---glossary-cwe-00363d2dcee93b006297.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xbce2e06f5845],{635:function(e,t){e.exports={data:{markdownRemark:{html:"<p>Common Weakness Enumeration (CWE) is the framework HackerOne utilizes to assign a weakness to a vulnerability.</p>",frontmatter:{path:"/glossary/cwe",title:"CWE"}}},pathContext:{}}}});
-//# sourceMappingURL=path---glossary-cwe-00363d2dcee93b006297.js.map
--- path---glossary-signal-70b263d64a38c882babe.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xecde2c08399c],{646:function(t,a){t.exports={data:{markdownRemark:{html:"<p>Average reputation gained per report.</p>",frontmatter:{path:"/glossary/signal",title:"Signal"}}},pathContext:{}}}});
-//# sourceMappingURL=path---glossary-signal-70b263d64a38c882babe.js.map
