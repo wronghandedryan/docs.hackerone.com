e0149f8a489c677f9db80e1587e62c8a4759da8c
Stacy Spiva <stacy@hackerone.com>
2019-05-22 16:45:53 -0700

    Updates

--- path---glossary-signal-96506a6c68baa4c1de25.js
+++ path---glossary-signal-96506a6c68baa4c1de25.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xecde2c08399c],{664:function(t,a){t.exports={data:{markdownRemark:{html:'<p>Average <a href="/hackers/reputation.html">reputation</a> gained per report.</p>',frontmatter:{path:"/glossary/signal",title:"Signal"}}},pathContext:{}}}});
+webpackJsonp([0xecde2c08399c],{665:function(t,a){t.exports={data:{markdownRemark:{html:'<p>Average <a href="/hackers/reputation.html">reputation</a> gained per report.</p>',frontmatter:{path:"/glossary/signal",title:"Signal"}}},pathContext:{}}}});
 //# sourceMappingURL=path---glossary-signal-96506a6c68baa4c1de25.js.map
