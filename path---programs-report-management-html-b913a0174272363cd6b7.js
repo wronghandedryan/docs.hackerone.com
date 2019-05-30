9bdf811c0b3949b847e604cff984cffe0cbe8afd
Stacy Spiva <stacy@hackerone.com>
2019-05-30 14:41:30 -0700

    Updates

--- path---programs-report-management-html-b913a0174272363cd6b7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xc87d8f4028c1],{545:function(t,r){t.exports={data:{markdownRemark:{html:"<p>Hackers submit vulnerabilities to your program through submitting reports. You can manage all incoming reports through your Inbox. </p>",frontmatter:{path:"/programs/report-management.html",id:"programs/report-management",title:"Report Management"}}},pathContext:{}}}});
-//# sourceMappingURL=path---programs-report-management-html-b913a0174272363cd6b7.js.map
