98fb9e6af1982807c30b838604123b22f8e77a42
Stacy Spiva <stacy@hackerone.com>
2019-07-02 11:14:11 -0700

    Updates

--- path---glossary-iso-30111-faa18d18e1ba980ece3e.js
+++ path---glossary-iso-30111-faa18d18e1ba980ece3e.js
@@ -1,2 +1,2 @@
-webpackJsonp([24686510598985],{662:function(t,a){t.exports={data:{markdownRemark:{html:"<p>An international standard describing vulnerability handling processes.</p>",frontmatter:{path:"/glossary/iso-30111",title:"ISO 30111"}}},pathContext:{}}}});
+webpackJsonp([24686510598985],{664:function(t,a){t.exports={data:{markdownRemark:{html:"<p>An international standard describing vulnerability handling processes.</p>",frontmatter:{path:"/glossary/iso-30111",title:"ISO 30111"}}},pathContext:{}}}});
 //# sourceMappingURL=path---glossary-iso-30111-faa18d18e1ba980ece3e.js.map
