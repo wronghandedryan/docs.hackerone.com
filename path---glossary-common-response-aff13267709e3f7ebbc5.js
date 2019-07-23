2a6d854e4b1f505433a26191648c5252de79b4be
Stacy Spiva <stacy@hackerone.com>
2019-07-23 14:11:12 -0700

    Updates

--- path---glossary-common-response-aff13267709e3f7ebbc5.js
+++ path---glossary-common-response-aff13267709e3f7ebbc5.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x8c86ee4aec93],{656:function(e,t){e.exports={data:{markdownRemark:{html:"<p>A saved response or template that can be applied repeatedly to reports. </p>",frontmatter:{path:"/glossary/common-response",title:"Common Response"}}},pathContext:{}}}});
+webpackJsonp([0x8c86ee4aec93],{657:function(e,t){e.exports={data:{markdownRemark:{html:"<p>A saved response or template that can be applied repeatedly to reports. </p>",frontmatter:{path:"/glossary/common-response",title:"Common Response"}}},pathContext:{}}}});
 //# sourceMappingURL=path---glossary-common-response-aff13267709e3f7ebbc5.js.map
