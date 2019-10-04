344ad79d7af8e78aefaf18fe47e51bef722d178c
Stacy Spiva <stacy@hackerone.com>
2019-10-04 11:56:00 -0700

    Updates

--- path---changelog-2017-april-c86a53fbfe435d641f7e.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([98390517608573],{486:function(e,t){e.exports={data:{markdownRemark:{html:"<h3>Personalized Invitation Messages</h3>\n<p>Hacker invitations can now be personalized with a personal message to the hacker(s) receiving the invitation. </p>",frontmatter:{path:"/changelog/2017/april",title:"April 2017"}}},pathContext:{}}}});
-//# sourceMappingURL=path---changelog-2017-april-c86a53fbfe435d641f7e.js.map
